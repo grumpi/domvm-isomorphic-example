e14c2791c1c070811588dca1f6bafd943c0e4115
grumpi <sabineschmaltz@gmail.com>
2016-05-02 08:58:15 +0000

    wrapping attaching into DOMContentLoaded

--- client.js
+++ client.js
@@ -4,9 +4,11 @@ var domvm = require('./../domvm');
 var app = new example_app.IsomorphicTestApp();
 var router = domvm.route(example_app.IsomorphicTestAppRouter, app);
 
-app.view.attach(document.body.firstChild);
+document.addEventListener("DOMContentLoaded", function() {
+  app.view.attach(document.body.firstChild);
 
-app.content = "Now the client has loaded.";
-app.view.redraw();
+  app.content = "Now the client has loaded.";
+  app.view.redraw();
 
-console.log("Client initialized!");
+  console.log("Client initialized!");
+});
